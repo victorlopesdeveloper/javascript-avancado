846f6ea99fe56a0d71707772cdff9e339e1b2b2f
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-06-11 11:07:08 -0300

    aula de variáveis completa

--- aulas/variaveis.js
+++ aulas/variaveis.js
@@ -1,7 +1,58 @@
 // Váriaveis no JavaScript
 
-const meuNome = 'Victor Lopes Moreira'
+// const let var // O VAR ESTÁ EM DESUSO POIS ELA NAO RESPEITA OS ESCOPO DE BLOCO E PODE 'VAZAR' PARA FORA, FAZENDO QUE QUE BUGUE O SISTEMA
 
-console.log(meuNome)
+//const meuNome = 'Victor Lopes Moreira'; // DEPOIS DE DECLARAR A CONSTANTE(const) NÃO PODE SER MODIFICADO O VALOR ATRIBUIDO A ELA.
+// let sobrenome = 'Lopes'; // let É UMA VARIÁVEL QUE PODE SER MODIFCADA DEPOIS DE DECLARADA.
+// AS CONVENÇÕES DE PROGRAMADORES, INDICAM QUE SEMPRE QUE TIVER UMA VÁRIAVEL QUE NÃO VAI SER MODIFICADA, DÊ PRIORIDADE AO USO DO const.
+/*
+TANTO const QUANTO let PODEM RECEBER TODOS ESSES TIPOS DE VALORES QUE TEM A SEGUIR:
 
-let nome = 'Victor L. Moreira'
+const string = 'Esse é um texto';
+const number = 28;
+const boolean = true;
+const array = [];
+const object = {};
+const func = function(){};
+const data = new Date();
+*/
+/*
+ MÁ PRÁTICA (NÃO RECOMENDO!)
+
+ -NOMES EM OUTROS IDIOMAS: DÊ PREFERÊNCIA A DECLARAVAR VARIÁVEIS COM NOMES EM INGLÊS
+ -ABREVIAÇÕES: TENTE ESCREVER O NOME COMPLETO DA VARÁVEL, EM VEZ DE ABREVIÁLAS
+ -ENUMERAÇÃO: NÃO USE NOMES PARA VARIÁVEIS (text1, text2) ESPECIFIQUE MELHOR CADA VARIÁVEL
+ -INCOERÊNCIA: DECLARE NOMES PARA AS VARIÁVEIS COMPATÍVEIS COM O QUE ELA RECEBE, NÃO COLOQUE NOMES INCOERÊNTES NAS VARIÁVEIS
+ */
+/*
+ BOAS PRÁTICAS
+
+-NOMES EM INGLÊS
+-EVITAR ENUMERAR
+-camelCase: ex(myName, yourName)
+-NOME EXPLICATIVO
+*/
+
+const myName = 'Victor';
+const mySecondName = 'Lopes';
+const myLastName = 'Moreira';
+const dateOfBirth = 1998;
+let myAge = 28;
+const myMother = 'Inês Moreira de Andrade Lopes';
+const myFather = 'José Lopes Batista';
+const myCityOfBirth = 'Triunfo'
+const iAmAMan = true;
+const iAmAGirl = false;
+let myWork = 'Assistente de Contabilidade';
+let myFutureWork = 'Desenvolvedor Full Stack';
+const mySister = 'Maria Victoria Lopes Moreira';
+let myHobbies = ['treinar', 'Jogar', 'Caminhar', 'Tocar Violão', 'Boxe', 'Assistir Séries']
+
+/*
+ PARA QUE FORAM CRIADAS E PARA QUE SERVEM AS VARIÁVEIS?
+ 
+ -FORAM CRIADAS PARA ARMAZENAR VALORES DENTRO DELAS, ELAS SERVEM COMO ETIQUETAS OU 'CAIXAS' 
+ PARA QUE OS PROGRAMAS CONSIGAM ACESSÁ-LAS, MODIFICÁ-LAS OU USÁ-LAS!
+ 
+*/ 
+
