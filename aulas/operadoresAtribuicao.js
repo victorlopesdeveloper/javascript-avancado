31b465fa43ce7a8d4d16e9eafcd803a607ad50aa
Victor Lopes <victorlopesdeveloper@gmail.com>
2026-06-13 17:19:49 -0300

    operadores de atribuição

---
+++ aulas/operadoresAtribuicao.js
@@ -0,0 +1,44 @@
+//OPERADORES DE ATRIBUIÇÃO
+/*
+ATRIBUIÇÃO SIMPLES:      =
+ADIÇÃO COMBINADA:        +=
+SUBTRAÇÃO COMBINADA:     -=
+MULTIPLICAÇÃO COMBINADA: *=
+DIVISÃO COMBINADA:       /=
+MODULAR COMBINADA:       %=
+EXPONENCIAÇÃO COMBINADA: **= 
+*/
+
+let nome = "Victor Lopes Moreira"; // (=) OPERADOR DE ATRIBUIÇÃO, SIGNIFICA RECEBE!
+
+console.log(nome);
+
+let preco = 300;
+
+preco += 50;//ADIÇÃO COMBINADA //A MESMA COISA DE preco = preço + 50; //RES: 350  
+console.log(preco)
+
+let preco2 = 500;
+
+preco2 -= 100;//SUBTRAÇÃO COMBINADA //A MESMA COISA DE preco2 = preco2 - 100; //RES: 400
+console.log(preco2);
+
+preco3 = 700;
+preco3 *= 5;//MULTIPLICAÇÃO COMBINADA //A MESMA COISA DE preco3 = preco3 * 5; //RES: 3 500
+console.log(preco3);
+
+let preco4 = 1000;
+
+preco4 /= 5;//DIVISÃO COMBINADA //A MESMA COISA DE preco4 = preco4 / 5; //RES: 200
+console.log(preco4);
+
+let preco5 = 10;
+
+preco5 %= 3;// MODULO COMBINADO // A MESMA COISA DE preco5 = preco5 % 3; //RES: 1
+console.log(preco5)
+
+let preco6 = 5;
+
+preco6 **= 3 // EXPONECIAÇÃO COMBINADA //A MESMA COISA DE preco6 = preco6 ** 3 // RES: 125 
+console.log(preco6)
+
