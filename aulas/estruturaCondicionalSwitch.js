99d5fe5ca6a941ce3772ffeba8f81e67fd58c3e5
Victor Lopes <victorlopesdeveloper@gmail.com>
2026-06-16 20:16:57 -0300

    finalizando o aprendizado sobre laços de repetição
    praticando o uso do switch

--- aulas/estruturaCondicionalSwitch.js
+++ aulas/estruturaCondicionalSwitch.js
@@ -101,8 +101,59 @@ const prompt = r1.createInterface({
 })
 
 console.log("Bem vindo ao programa");
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
+console.log("[1] Data atual");
+console.log("[2] Horário atual");
+console.log("[3] Ver animais");
+console.log("[4] Ver comidas");
+console.log("[5] Ver linguagens");
+console.log("[0] Sair");
+
+prompt.question("Digite a opção escolhida: ", pergunta => {
+    switch(pergunta){
+        case "1": {
+            const data = new Date();
+            const dia = data.getDate();
+            const mes = data.getMonth()+1;
+            const ano = data.getFullYear();
+            console.log(`Hoje é dia ${dia}/${mes}/${ano}`)
+            break;
+        }
+        case "2": {
+            const horario = new Date();
+            const segundo = horario.getSeconds();
+            const minuto = horario.getMinutes();
+            const hora = horario.getHours();
+            console.log(`Agpra são ${hora}:${minuto}:${segundo} hrs`);
+            break;
+        }
+        case "3": {
+            console.log("Tigre");
+            console.log("Leão");
+            console.log("Cachorro");
+            console.log("Gato");
+            console.log("Pato");
+            break;
+        }
+        case "4": {
+            console.log("Pizza");
+            console.log("Hamburguer");
+            console.log("Macarronada");
+            console.log("Lasanha");
+            console.log("Camarão");
+            console.log("Churrasco");
+            break;
+        }
+        case "5": {
+            console.log("JavaScript");
+            console.log("Phyton");
+            console.log("C++");
+            console.log("Java");
+            console.log("Laravel");
+            break;
+        }
+        default:{
+            console.log("Fim do programa")
+        }
+    }
+    prompt.close()
+})
