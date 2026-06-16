2c0cdbc7a8ff3dac7013d29b96dc54acce4f5d9b
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-06-16 10:18:46 -0300

    PRATICANDO OPERADORES TERINÁRIOS

---
+++ aulas/operadorTernario.js
@@ -0,0 +1,53 @@
+// OPERADOR TERNÁRIO
+
+// (expressão) ? 'valor se verdadeiro' : 'valor se falso'
+
+let idade = 19
+
+console.log(
+    idade < 18 ? "Você é menor de idade!" : 'Você é maior de idade!'
+)
+//--------------------------------------------------------------------------
+
+let notaDaProva = 69;
+
+console.log(notaDaProva >= 70 ? 'Você foi aprovado' : 'Você foi reprovado!')
+
+//--------------------------------------------------------------------------
+
+let saldoDoBanco = 500;
+let transferenciaBancaria = 550;
+
+console.log(transferenciaBancaria <= saldoDoBanco ? "Transferência concluída!" : "Saldo insuficiente")
+
+
+//--------------------------------------------------------------------------
+
+let obrigacoes = true;
+let treinar = true;
+let alimentacao = false;
+
+
+
+console.log(
+    obrigacoes == true && treinar == true && alimentacao == true
+    ? 'Posso ficar de bobeira'
+    : 'Tenho coisas para fazer'
+)
+
+//----------------------------------------------------------------------------
+
+let horas = 13;
+
+console.log(
+    horas >= 0 && horas < 6 
+    ? "Está de madrugada"
+    : horas >= 6 && horas <= 12
+    ? "Está de dia"
+    : horas <= 18
+    ? "Está de tarde"
+    : horas <= 24
+    ? "Está de noite"
+    : "Esse horário não existe!"
+)
+//-----------------------------------------------------------------------------
