af1140374780ee5dbd5868f4c3f0bf76223ab3f3
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-07-02 15:21:32 -0300

    aprendendo o uso de modulos

---
+++ aulas/modulos/app.js
@@ -0,0 +1,6 @@
+import {somar, multiplicar} from './math.js';
+import dividir from "./division.js"
+
+console.log(somar(5, 3));
+console.log(multiplicar(5, 10));
+console.log(dividir(50, 10));
---
+++ aulas/modulos/math.js
@@ -0,0 +1,5 @@
+export const somar = (a,b) => a + b;
+
+export function multiplicar (a,b) {
+    return a * b;
+}
