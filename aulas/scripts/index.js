256fb99872780def4371686c9acef8443923e684
victor-lopes-developer <victorlopesdeveloper@gmail.com>
2026-07-06 14:08:37 -0300

    usando scripts

---
+++ aulas/scripts/index.js
@@ -0,0 +1,13 @@
+// SCRIPTS
+
+/*
+console.log("Hello World!");
+
+console.log(process.env.EMAIL);
+console.log(process.env.PASSWORD);
+*/
+
+function soma (a,b){
+    return a + b;
+}
+console.log(soma(5,10));
